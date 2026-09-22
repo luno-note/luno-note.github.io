e768eb600a04fd821428568c2999827c8faa45a8
phanuwatla <phanuwat.l@kkumail.com>
2026-09-23 04:32:36 +0700

    Add GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   plugins: [react()],
+  base: '/luno-site/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
